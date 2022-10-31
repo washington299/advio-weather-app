aca71b31f4d29d36933380a655fadeb569880c85
Washington Campos <washingtoncampos9@gmail.com>
2022-10-31 19:31:52 -0300

    test: ensure Main renders elements correctly

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+
+import { globalRender } from 'tests/helpers';
+
+import { Main } from '.';
+
+const useGetCityWeather = jest.spyOn(require('services/queries'), 'useGetCityWeather');
+const mutate = jest.fn();
+
+useGetCityWeather.mockImplementation(() => ({
+	mutate,
+}));
+
+describe('<Main />', () => {
+	it('should render Main with correct elements', () => {
+		globalRender(<Main />);
+
+		expect(screen.getByText(/No city selected/i)).toBeInTheDocument();
+		expect(screen.getByRole('combobox')).toBeInTheDocument();
+		expect(screen.getByRole('switch')).toBeInTheDocument();
+	});
+});
