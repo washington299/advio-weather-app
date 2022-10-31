b642509d50ea5e13973ce6a1280ca255925e605a
Washington Campos <washingtoncampos9@gmail.com>
2022-10-31 18:26:52 -0300

    feat: add units param on getCityWeather function request

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -2,8 +2,8 @@ import { useMutation } from '@tanstack/react-query';
 
 import { api } from 'services/config';
 
-const getCityWeather = async (city: string) => {
-	const { data } = await api.get(`/weather?appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&q=${city}`);
+const getCityWeather = async (city: string, units = 'metric') => {
+	const { data } = await api.get(`/weather?appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}&q=${city}&units=${units}`);
 
 	return data;
 };
