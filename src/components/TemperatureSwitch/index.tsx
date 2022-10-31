21b1291c840b8c6e46d6a5531ee066f160c57321
Washington Campos <washingtoncampos9@gmail.com>
2022-10-31 15:31:20 -0300

    feat: create TemperatureSwitch component

---
+++ src/components/TemperatureSwitch/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import WrongSwitch from "react-switch";
+
+import * as S from './styles';
+
+// apparently TS has an issue with the return type of Switch so in this case i had to use any to avoid ts error
+const Switch = WrongSwitch as any;
+
+type TemperatureSwitchProps = {
+	handleSwitchChange: (value: boolean) => void;
+};
+
+export const TemperatureSwitch = ({ handleSwitchChange }: TemperatureSwitchProps) => {
+	const [checked, setChecked] = useState(false);
+
+	const handleChange = (nextChecked: boolean) => {
+		setChecked(nextChecked);
+		handleSwitchChange(nextChecked);
+	};
+
+	return (
+		<S.Label htmlFor='switch'>
+			<S.Text>°C</S.Text>
+			<Switch
+				id='switch'
+				className="switch"
+				checked={checked}
+				onChange={handleChange}
+				offColor="#4A5568"
+				onColor="#4A5568"
+				offHandleColor="#DD6B20"
+				onHandleColor="#DD6B20"
+				handleDiameter={22}
+				uncheckedIcon={false}
+				checkedIcon={false}
+				width={40}
+				height={15}
+			/>
+			<S.Text>°F</S.Text>
+		</S.Label>
+	);
+};
---
+++ src/components/TemperatureSwitch/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+	display: flex;
+	align-items: center;
+
+	.switch {
+		margin: 0 var(--size-xs);
+	}
+`;
+
+export const Text = styled.span`
+	color: var(--white);
+`;
