27bda741608d9ef3ad5d3a6f3eacc5d4b274e3d7
Washington Campos <washingtoncampos9@gmail.com>
2022-11-10 16:22:21 -0300

    test: ensure formatTemperature returns correct values with both temperatures units

---
+++ src/utils/formatTemperatureUnit/test.ts
@@ -0,0 +1,21 @@
+import { formatTemperatureUnit } from './index';
+
+describe('formatTemperatureUnit', () => {
+	it('Should return temperature with celsius unit when isCelsius param is true', () => {
+		const value = 15;
+		const isCelsius = true;
+
+		const response = formatTemperatureUnit(value, isCelsius);
+
+		expect(response).toBe('15 °C');
+	});
+
+	it('Should return temperature with fahrenheit unit when isCelsius param is false', () => {
+		const value = 15;
+		const isCelsius = false;
+
+		const response = formatTemperatureUnit(value, isCelsius);
+
+		expect(response).toBe('15 °F');
+	});
+});
