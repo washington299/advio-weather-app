bd1ed02b472b3d9c1dedf29d2d1724015916b579
Washington Campos <washingtoncampos9@gmail.com>
2022-11-10 19:41:27 -0300

    feat: replace react-switch library with custom css

--- src/components/TemperatureSwitch/index.tsx
+++ src/components/TemperatureSwitch/index.tsx
@@ -1,22 +1,78 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import WrongSwitch from "react-switch";
-
-// apparently TS has an issue with the return type of Switch so in this case i had to use any to avoid ts error
-const Switch = WrongSwitch as any;
-
-const Label = styled.label`
+const Content = styled.div`
 	display: flex;
 	align-items: center;
-
-	.switch {
-		margin: 0 var(--size-xs);
-	}
 `;
 
-const Text = styled.span`
+const Label = styled.label`
 	color: var(--white);
+	margin: 0 var(--size-sm);
+`;
+
+const SwitchLabel = styled.label<{ isDisabled: boolean }>`
+	position: relative;
+	display: inline-block;
+	width: 40px;
+	height: 15px;
+
+	${({ isDisabled }) => css`
+		opacity: ${isDisabled ? 0.6 : 1};
+	`}
+
+	input {
+		opacity: 0;
+		width: 0;
+		height: 0;
+	}
+
+	.slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: var(--dark-gray);
+		-webkit-transition: .4s;
+		transition: .4s;
+	}
+
+	.slider:before {
+		position: absolute;
+		content: "";
+		height: 22px;
+		width: 22px;
+		left: -4px;
+		bottom: -3px;
+		background-color: var(--white);
+		-webkit-transition: .4s;
+		transition: .4s;
+		background-color: var(--orange);
+	}
+
+	input:checked + .slider {
+		background-color: var(--dark-gray);
+	}
+
+	input:focus + .slider {
+		box-shadow: 0 0 1px var(--orange);
+	}
+
+	input:checked + .slider:before {
+		-webkit-transform: translateX(26px);
+		-ms-transform: translateX(26px);
+		transform: translateX(26px);
+	}
+
+	.slider.round {
+		border-radius: 34px;
+	}
+
+	.slider.round:before {
+		border-radius: 50%;
+	}
 `;
 
 type TemperatureSwitchProps = {
@@ -27,31 +83,30 @@ type TemperatureSwitchProps = {
 export const TemperatureSwitch = ({ disabled, handleSwitchChange }: TemperatureSwitchProps) => {
 	const [checked, setChecked] = useState(false);
 
-	const handleChange = (nextChecked: boolean) => {
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const nextChecked = e.target.checked;
+
 		setChecked(nextChecked);
 		handleSwitchChange(nextChecked);
 	};
 
 	return (
-		<Label htmlFor='switch'>
-			<Text>°C</Text>
-			<Switch
-				id='switch'
-				className="switch"
-				disabled={disabled}
-				checked={checked}
-				onChange={handleChange}
-				offColor="#4A5568"
-				onColor="#4A5568"
-				offHandleColor="#DD6B20"
-				onHandleColor="#DD6B20"
-				handleDiameter={22}
-				uncheckedIcon={false}
-				checkedIcon={false}
-				width={40}
-				height={15}
-			/>
-			<Text>°F</Text>
-		</Label>
+		<Content>
+			<Label htmlFor="toggle">°C</Label>
+
+			<SwitchLabel className="switch" isDisabled={disabled}>
+				<input
+					id="toggle"
+					type="checkbox"
+					role="switch"
+					disabled={disabled}
+					checked={checked}
+					onChange={handleChange}
+				/>
+				<span className="slider round"></span>
+			</SwitchLabel>
+
+			<Label htmlFor="toggle">°F</Label>
+		</Content>
 	);
 };
